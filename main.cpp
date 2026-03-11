56055bc67410987da2b961ab35cca647764d5865
Kyu Lee <kwooler@gmail.com>
2026-03-11 01:23:20 -0700

    Student starter code

--- main.cpp
+++ main.cpp
@@ -6,15 +6,6 @@ int main()
 {
 	int prev, current;
 	// TODO
-	int flag = 1;
 
-	cin >> current;
-	while ((flag == 1) or (prev > current))
-	{
-		flag = 0;
-		cout << current << "\t";
-		prev = current;
-		cin >> current;
-	}
 	// TODO
 }
