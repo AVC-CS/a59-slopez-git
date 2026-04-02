6953804fe916e8313892affa533090e37c264717
Saml <slopez152@students.avc.edu>
2026-04-02 16:33:07 -0700

    A59

--- main.cpp
+++ main.cpp
@@ -4,8 +4,15 @@ using namespace std;
 
 int main()
 {
-	int prev, current;
-	// TODO
-
-	// TODO
+    int prev, current;
+    int flag = 1;
+    cin >> current;
+    while ((flag == 1) || (prev > current))
+    {
+        flag = 0;
+        cout << current << "\t";
+        prev = current;
+        cin >> current;
+    }
+    cout << endl;
 }
